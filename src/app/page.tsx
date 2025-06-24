e34a40d0e5fde8d350573ac7d84d10c0e73d3946
shijayas <anjithashijay@gmail.com>
2025-06-24 17:52:09 +0530

    05: authentication socials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,55 +1,20 @@
-"use client";
-
-import { Button } from "@/components/ui/button";
-import { useState } from "react";
-import { authClient } from "@/lib/auth-client";
-
-import { useRouter } from "next/navigation";
+import { auth } from "@/lib/auth";
+import HomeView from "@/modules/home/ui/views/home-view";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 
-export default function Home() {
-  const {data:session} = authClient.useSession();
+const page = async () => {
 
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const session = await auth.api.getSession({
+    headers: await headers()
+  });
 
-  const router = useRouter();
-
-  const onSubmit = async () => {
-  authClient.signUp.email({
-    email,
-    password,
-    name  
-  },{
-    onError: (e) => {
-      console.log(e);
-      window.alert("something went wrong");
-    },
-    onSuccess: () => {
-      window.alert("success");
-    }
-  })
-  };
-
-  const onLogin = async () => {
-    authClient.signIn.email({
-      email,
-      password
-    })
+  if(!session) {
+  redirect("/sign-in")
   }
 
-  if(session) {
-    return (
-      <div className="flex flex-col gap-y-10"> 
-        <h1>hello {session.user?.name}</h1>
-        <button onClick={() => authClient.signOut()}>Sign out</button>
-      
-      </div>
-    )
-  }else{
-   router.push("/sign-in")
-  }
-
-
+  return <HomeView/>
 }
+
+export default page
